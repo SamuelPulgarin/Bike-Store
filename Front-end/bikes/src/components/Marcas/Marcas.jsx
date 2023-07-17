191fffe6ab958b45c1c1cc4312aac715c255f101
Ricardo <rpolaniarubiano@gmail.com>
2023-07-17 17:43:01 -0500

    Avances en maquetado cards category

---
+++ Front-end/bikes/src/components/Marcas/Marcas.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import '../../assets/css/marcas.css'
+import scott from '../../assets/img/Marcas/scott.png'
+import gw from '../../assets/img/Marcas/gw.png'
+import giant from '../../assets/img/Marcas/giant.png'
+import trek from '../../assets/img/Marcas/trek.png'
+
+export const Marcas = () => {
+    return (
+        <>
+            <h2 className='titulo-marcas'>Marcas:</h2>
+
+            <main className='section-marcas'>
+                <div className='marca1'>
+
+                </div>
+                <div className='marca2'>
+
+                </div>
+                <div className='marca3'>
+
+                </div>
+                <div className='marca4'>
+
+                </div>
+            </main>
+        </>
+    )
+}
---
+++ Front-end/bikes/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { CardsCategory } from '../../components/Category-Cards/CardsCategory'
+import { Marcas } from '../../components/Marcas/Marcas'
+
+export const Home = () => {
+  return (
+    <>
+        <CardsCategory></CardsCategory>
+        <Marcas></Marcas>
+    </>
+  )
+}
