8b7870fb9e7e530b7a3c5d53e89aad81d4d89ed7
Lord17777 <canegrn@misena.edu.co>
2023-07-18 17:44:27 -0500

    Hola, se termino el diseño de la barra de busqueda.

--- Front-end/bikes/src/components/BannerAndSearch/Banner_search.jsx
+++ Front-end/bikes/src/components/BannerAndSearch/Banner_search.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import OficialBanner from '../../assets/img/BannerOficial.png';
 import '../../assets/CSS/Banner_search_styles.css';
+import Lupa from '../../assets/img/Lupa.png'
 
 const Banner_search = () => {
     return (
         <>
-
-            <div className='MainBanner'>
-                <img src={OficialBanner} alt="Banner" />
-            </div>
-            <div className='Search'>
-                <input type="text" placeholder='Buscar' />
+            <div className='Container'>
+                <div className='Search'>
+                    <input type="text" placeholder='    Buscar' className='search'/>
+                    <button id='lupa'><img src={Lupa} alt="Lupa"/></button>
+                </div>
             </div>
         </>
     )
