b971510301af5a07c5128638276bfec8e99ce6f7
Lord17777 <canegrn@misena.edu.co>
2023-07-21 01:05:12 -0500

    Avances del diseño del Footer

--- Front-end/bikes/src/App.jsx
+++ Front-end/bikes/src/App.jsx
@@ -1,9 +1,10 @@
 import Banner_search from '../src/components/BannerAndSearch/Banner_search';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <>
-      <Banner_search/>
+      <Footer/>
     </>
   )
 }
