f813db0702907307329c9ddd54f6f4842dd71b5c
Lord17777 <canegrn@misena.edu.co>
2023-07-21 17:42:14 -0500

    Sólo queda el responsive

--- Front-end/bikes/src/components/Footer/Footer.jsx
+++ Front-end/bikes/src/components/Footer/Footer.jsx
@@ -3,35 +3,33 @@ import "../../assets/CSS/Footer.css";
 import Logo from "../../assets/img/Logo-BikeStore.svg";
 import Copy from "../../assets/img/IconoCopy.svg";
 
+
+
 const Footer = () => {
   return (
     <body>
       <div className="FooterContain">
-        <div className="Follow_us">
-          <h2>SIGUENOS EN</h2>
-            <a href="#">
-              <i className="Facebook">
-              </i>
-            </a>
-            <a href="#">
-              <i className="Instagram">
-              </i>
-            </a>
-            <a href="#">
-              <i className="Twitter">
-              </i>
-            </a>
-            <a href="#">
-              <i className="Youtube">
-              </i>
-            </a>
-        </div>
         <div className="Logo">
           <img src={Logo} alt="Logo" />
         </div>
+        <div className="Follow_us">
+          <h2>SIGUENOS EN</h2>
+          <a href="#">
+            <i class="fa-brands fa-facebook"></i>
+          </a>
+          <a href="#">
+            <i class="fa-brands fa-instagram"></i>
+          </a>
+          <a href="#">
+            <i class="fa-brands fa-twitter"></i>
+          </a>
+          <a href="#">
+            <i class="fa-brands fa-youtube"></i>
+          </a>
+        </div>
         <div className="Copy">
-          <h3>2023 Bike Store. Todos los derechos reservados</h3>
           <img src={Copy} alt="Imagen de Copyright" />
+          <h3>2023 Bike Store. Todos los derechos reservados</h3>
         </div>
       </div>
     </body>
