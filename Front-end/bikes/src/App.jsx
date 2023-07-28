c25207bae7b8605ac70519c9d8f9fcd030350b84
Samuel <samuel@gmail.com>
2023-07-28 10:24:38 -0500

    inicializar Home en app.jsx y arreglo de bug de duplicacion al redireccionar

--- Front-end/bikes/src/App.jsx
+++ Front-end/bikes/src/App.jsx
@@ -7,22 +7,21 @@ import Navbar from './components/navbar/Navbar.jsx';
 import Productos from './pages/Productos.jsx';
 import { Signin_Signup } from './components/Signin-SignUp/Signin_Signup';
 
+
 function App() {
 
   return (
     <>
 
       <Router>
-      <Navbar/>
+        <Home/>
         <Routes>
-          <Route path='/home' element={<Home />}/>
           <Route path='/Carrito' element={<Carrito />} />
           <Route path='/productos' element={<Productos/>}/>
           <Route path='/perfil' element={<Signin_Signup/>} />
         </Routes>
       </Router>
       
-      <Footer/>
     </>
 
   )
