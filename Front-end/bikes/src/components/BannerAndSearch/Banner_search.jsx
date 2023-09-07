f37cacfbc2e394b7eef5a22d42b0c5d7bc950e72
Lord17777 <canegrn@misena.edu.co>
2023-09-07 18:16:52 -0500

    Comienzo de la lógica del buscador

--- Front-end/bikes/src/components/BannerAndSearch/Banner_search.jsx
+++ Front-end/bikes/src/components/BannerAndSearch/Banner_search.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import '../../assets/CSS/Banner_search_styles.css';
 import Lupa from '../../assets/img/Lupa.png'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 /*ja */
 const Banner_search = () => {
+
+    const [bikes, setBikes] = useState([]);
+    const [search, setSearch] = useState("");
+
+    const peticiónGet = async() => {
+        await axios.get("http://localhost:3060")
+        .then(response => {
+            console.log(response.data)
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+
+    useEffect(() => {
+        peticiónGet();
+    }, [])
+
+
     return (
         <>
             <div className='Container'>
