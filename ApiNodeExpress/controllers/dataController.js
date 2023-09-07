3eb76f722efbfb120167141289d5d02c7abee95c
Samuel <samuel@gmail.com>
2023-09-07 17:51:34 -0500

    actualizando API

--- ApiNodeExpress/controllers/dataController.js
+++ ApiNodeExpress/controllers/dataController.js
@@ -1,58 +1,60 @@
 const { Pool } = require("pg");
 const { CONFIG_BD } = require("../config/db");
 
-//aqui empieza el sistema de logs
-const fs = require("fs");
-const path = require("path");
-
-function generateLog(text, logDirectory) {
-  const logFilePath = path.join(logDirectory, "app.log");
-  const timestamp = new Date().toISOString();
-  const log = `[${timestamp}] ${text}\n`;
-
-  fs.appendFile(logFilePath, log, (err) => {
-    if (err) {
-      console.error("Error al escribir en el archivo de registro:", err);
-    }
-  });
-}
-
-function logError(error, logDirectory) {
-  const errorMessage = `ERROR: ${error.message}\nStack: ${
-    error.stack || "N/A"
-  }`;
-  generateLog(errorMessage, logDirectory);
-}
-
-const logDirectory = path.join(__dirname, "logs");
-
-// Asegurarse de que el directorio de logs exista
-if (!fs.existsSync(logDirectory)) {
-  fs.mkdirSync(logDirectory);
-}
-//aqui termina el sistema de logs
-/*
-// Ejemplo de uso
-try {
-    // Tu código que puede generar errores
-    throw new Error('Este es un error de ejemplo');
-  } catch (error) {
-    logError(error, logDirectory);
-  }
-  */
 const pool = new Pool(CONFIG_BD);
 console.log(pool);
 
 const getProducts = (req, res) => {
-  pool.query("SELECT producto.*, imagenes.ruta FROM producto JOIN imagenes ON producto.id = imagenes.id;", (error, result) => {
-    if (error) {
-      console.error("Error al obtener los datos", error.message);
-      res.status(500).send("Error al obtener datos");
-    } else {
-      res.json(result.rows);
+  pool.query(
+    "SELECT producto.*, imagenes.ruta FROM producto JOIN imagenes ON producto.id = imagenes.id;",
+    (error, result) => {
+      if (error) {
+        console.error("Error al obtener los datos", error.message);
+        res.status(500).send("Error al obtener datos");
+      } else {
+        res.json(result.rows);
+      }
     }
-  });
+  );
 };
+
+const getProductsByMarca = async(req, res) =>{
+
+  const marca = req.params.marca
+
+  try {
+    // Realiza una consulta a la base de datos para obtener el producto por su Marca
+    const product = await pool.query("SELECT * FROM producto WHERE marca = $1", [marca]);
+
+    if (product.rows.length === 0) {
+      return res.status(404).json({ error: "Producto no encontrado" });
+    }
+
+    res.status(200).json(product.rows[0]); // Devuelve los productos encontrado
+  } catch (error) {
+    console.error("Error al obtener producto por su Marca", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+
+const getProductById = async(req, res) =>{
+
+  const id = req.params.marca
+
+  try{
+    //Realiza una consulta a la base de datos para obtener el producto por su Marca
+    const product = await pool.query("SELECT * FROM producto WHERE id = $1", [id]);
+
+    if(product.rows.length === 0){
+      return res.status(404).json({ error: "Producto no encontrado"});
+    }
+
+    res.status(200).json(product.rows[0]); //Devolver producto encontrado
+  } catch(error){
+    console.error("Error al obtener producto por su Id", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
 
 /*const getImages = (req, res) => {
   pool.query("SELECT * FROM imagenes", (error, result) => {
@@ -98,97 +100,208 @@ const uploadImage = (req, res) => {
 };
 
 const registrerUser = (req, res) => {
-  try {
-    console.log(
-      "estoy aquii wachoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo"
-    );
-    const { username, email, password } = req.body;
+  console.log(
+    "estoy aquii wachoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo"
+  );
+  const { username, email, password } = req.body;
 
-    console.log("datos usuario", req.body);
+  console.log("datos usuario", req.body);
 
-    //Validar que todos los campos necesarios esten presentes
-    if (!username || !email || !password) {
-      return res.status(400).json({ error: "Falta la informacion requerida" });
-    }
-
-    // Consultar si el usuario ya existe en la base de datos
-    pool.query(
-      "SELECT * FROM usuarios WHERE username = $1 OR email = $2",
-      [username, email],
-      (error, result) => {
-        if (error) {
-          console.error("Error al consultar la base de datos", error);
-          return res
-            .status(500)
-            .json({ error: "Error al registrar el usuario", error });
-        }
-
-        //Si el usuario ya existe devolver un error
-        if (result.rows.length > 0) {
-          return res.status(400).json({ error: "el usuario ya existe" });
-        }
-
-        //Si el usuario no existe, insertar el nuevo usuario en la base de datos
-        pool.query(
-          "INSERT INTO usuarios (username, email, password) VALUES ($1, $2, $3)",
-          [username, email, password],
-          (error) => {
-            if (error) {
-              console.error(
-                "Error al insertar el usuario en la base de datos",
-                error
-              );
-              return res
-                .status(500)
-                .json({ error: "Error al registrar el Usuario" });
-            }
-
-            res
-              .status(201)
-              .json({ message: "Usuario registrado exitosamente" });
-          }
-        );
-      }
-    );
-  } catch (error) {
-    logError(error, logDirectory);
+  //Validar que todos los campos necesarios esten presentes
+  if (!username || !email || !password) {
+    return res.status(400).json({ error: "Falta la informacion requerida" });
   }
+
+  // Consultar si el usuario ya existe en la base de datos
+  pool.query(
+    "SELECT * FROM usuarios WHERE username = $1 OR email = $2",
+    [username, email],
+    (error, result) => {
+      if (error) {
+        console.error("Error al consultar la base de datos", error);
+        return res
+          .status(500)
+          .json({ error: "Error al registrar el usuario", error });
+      }
+
+      //Si el usuario ya existe devolver un error
+      if (result.rows.length > 0) {
+        return res.status(400).json({ error: "el usuario ya existe" });
+      }
+
+      //Si el usuario no existe, insertar el nuevo usuario en la base de datos
+      pool.query(
+        "INSERT INTO usuarios (username, email, password) VALUES ($1, $2, $3)",
+        [username, email, password],
+        (error) => {
+          if (error) {
+            console.error(
+              "Error al insertar el usuario en la base de datos",
+              error
+            );
+            return res
+              .status(500)
+              .json({ error: "Error al registrar el Usuario" });
+          }
+
+          res.status(201).json({ message: "Usuario registrado exitosamente" });
+        }
+      );
+    }
+  );
 };
 
-
 const inicioSesion = (req, res) => {
-
-  const { email, password} = req.body;
-  console.log("datos para el login: ", req.body)
+  const { email, password } = req.body;
+  console.log("datos para el login: ", req.body);
 
   //Validar que todos los campos necesarios esten presentes
   if (!email || !password) {
     return res.status(400).json({ error: "Falta la informacion requerida" });
   }
 
-
-  pool.query("SELECT * FROM usuarios WHERE email = $1 AND password = $2",[email, password], (error, result) => {
-    if (error) {
-      console.error("Error al obtener los datos", error.message);
-      res.status(500).send("Error al obtener datos");
-    } else {
-      if(result.rows.length > 0){
-        return res.json({ message: "Las credenciales coinciden, usuario valido"})
-      } else{
-        return res.status(401).json({ error: "Las credenciales no coinciden, usuario invalido"})
+  pool.query(
+    "SELECT * FROM usuarios WHERE email = $1 AND password = $2",
+    [email, password],
+    (error, result) => {
+      if (error) {
+        console.error("Error al obtener los datos", error.message);
+        res.status(500).send("Error al obtener datos");
+      } else {
+        if (result.rows.length > 0) {
+          return res.json({
+            message: "Las credenciales coinciden, usuario valido",
+          });
+        } else {
+          return res
+            .status(401)
+            .json({ error: "Las credenciales no coinciden, usuario invalido" });
+        }
       }
+
+      //Verificar que el usuario ya existe y confirmar la informacio
+    }
+  );
+};
+
+const addProduct = async (req, res) => {
+  console.log("estoy aquí wachaaaaaa muchachaaaaaaaaaaaaaaaaaaaaaa");
+
+  // Obtener los datos del producto
+  const {
+    id,
+    nombre,
+    marca,
+    descripcion,
+    categoria,
+    talla,
+    precio,
+    color,
+    stock,
+  } = req.body;
+  console.log("datos producto", req.body);
+  console.log("Ruta imagen: ", req.file);
+
+  // Obtener la ruta de la imagen subida
+  const rutaImagen = req.file ? `uploads/${req.file.filename}` : null;
+  console.log(rutaImagen);
+
+  // Validar que todos los campos necesarios estén presentes
+  if (
+    !id ||
+    !nombre ||
+    !marca ||
+    !descripcion ||
+    !categoria ||
+    !talla ||
+    !precio ||
+    !color ||
+    !stock
+  ) {
+    console.log('Me desvié por aquí');
+    return res.status(400).json({ error: "Falta la información requerida" });
+  }
+
+  try {
+    console.log('entre al try carajo');
+
+    // Consultar si el producto ya existe en la base de datos
+    const result = await pool.query(
+      "SELECT * FROM producto WHERE id = $1 OR nombre = $2",
+      [id, nombre]
+    );
+
+    if (result.rows.length > 0) {
+      return res.status(400).json({ error: "el producto ya existe" });
+    }
+    console.log('avanzando');
+
+    // Si el producto no existe, insertar el nuevo producto en la base de datos
+    await pool.query(
+      "INSERT INTO producto (id, nombre, marca, descripcion, categoria, talla, precio, color, stock) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)",
+      [
+        id,
+        nombre,
+        marca,
+        descripcion,
+        categoria,
+        talla,
+        precio,
+        color,
+        stock,
+      ]
+    );
+
+    console.log('casi termino');
+
+    // Insertar imagen con id del producto
+    if (rutaImagen) {
+      console.log('Me encuentro adentro de esto');
+      await pool.query("INSERT INTO imagenes (id, ruta) VALUES ($1, $2)", [
+        id,
+        rutaImagen,
+      ]);
     }
 
-    //Verificar que el usuario ya existe y confirmar la informacio
-    
+    console.log('lo logré');
 
-  });
+    res.status(201).json({ message: "Imagen y producto subidos exitosamente" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error en el servidor" });
+  }
 };
+
+
+const deleteProduct = async(req, res) =>{
+
+  const productId = parseInt(req.params.id);
+
+  try{
+
+    //Eliminar un producto por su id en la base de datos
+    const results = await pool.query("DELETE FROM producto WHERE id = $1", [productId]);
+
+    if(results.rowCount === 1){
+      res.status(200).json({ Message: 'Producto eliminado con exito'});
+    }else {
+      res.status(404).json({Message: 'Producto no encontrado'});
+    }
+  } catch(error){
+    console.error("Error al eliminar el producto", error);
+    res.status(500).json({error: 'Algo ha ocurrido cuando se ha intentado eliminar el recurso'});
+  }
+
+}
 
 module.exports = {
   getProducts,
   uploadImage,
-//  getImages,
+  //  getImages,
   registrerUser,
   inicioSesion,
+  addProduct,
+  getProductsByMarca,
+  deleteProduct,
+  getProductById,
 };
--- ApiNodeExpress/routes/dataRoutes.js
+++ ApiNodeExpress/routes/dataRoutes.js
@@ -3,8 +3,15 @@ const router = express.Router();
 const dataController = require('../controllers/dataController');
 const upload = require('../multerConfig'); //importar el middleware de Multer
 
-//Ruta para productos
+//Ruta para traer todos los productos
 router.get('/productos', dataController.getProducts);
+
+//Ruta para traer los productos por marca
+router.get('/productos/marca', dataController.getProductsByMarca);
+
+//Ruta para traer los productos por su Id
+router.get('/get-products/:id', dataController.getProductById);
+
 
 //Ruta para imagenes de los productos
 //router.get('/imagenes', dataController.getImages);
@@ -17,6 +24,14 @@ router.post('/registrar', dataController.registrerUser);
 
 //Ruta para el ingreso de usuarios
 router.post('/login', dataController.inicioSesion);
+
+//Ruta para agregar nuevos productos a la base de datos
+router.post('/create-product', upload.single('imagen'), dataController.addProduct);
+
+//Ruta para eliminar productos por su id
+router.delete("/delete-products/:id", dataController.deleteProduct);
+
+
 
 
 
