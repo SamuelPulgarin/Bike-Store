a3f4dd9a85eba5609b5bfbe3725ac2b6ed92293a
Samuel <samuel@gmail.com>
2023-09-13 17:40:06 -0500

    stock y cantidad de los productos agregados al carrito finalizado (carrito de compras en general)

--- Front-end/bikes/src/components/Shopping_Cart/CartaProducto.jsx
+++ Front-end/bikes/src/components/Shopping_Cart/CartaProducto.jsx
@@ -1,19 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../../assets/css/CartaProducto.css";
 import Example from "../../../uploads/1694211030597-BicicletaGiantPropelAdvancedPro0AXS23.jpg";
 import { useDispatch, useSelector } from "react-redux";
 import { ProductNotFoundInCart } from "./ProductNotFoundInCart";
 import { ButtomContinueBuy } from "./ButtomContinueBuy";
-import { removeItemFromCart, clearCart } from "../../redux/carritoSlice";
+import { removeItemFromCart, clearCart, updateStock } from "../../redux/carritoSlice";
 
 const CartaProducto = () => {
-  const [cantidad, setCantidad] = useState(1);
+
+  const [quantityArticle, setQuantityArticle] = useState(1);
 
   const cartItems = useSelector((state) => state.carrito.items);
   const dispatch = useDispatch();
   //console.log(cartItems);
 
-  const deleteProduct = (id)=>{
+  const deleteProduct = (id) => {
 
     console.log(id);
 
@@ -21,54 +22,71 @@ const CartaProducto = () => {
     dispatch(removeItemFromCart(numericId));
   }
 
-  if(!cartItems || cartItems.length === 0){
+  const handleStockChange = (id, newCantidad) => {
 
-    return(
+    console.log("ID:", id); // Verifica que id sea el ID correcto
+    console.log("Nuevo Stock:", newCantidad); // Verifica que stock sea el valor correcto
+    dispatch(updateStock({ id, cantidad: newCantidad }));
+
+  };
+
+  if (!cartItems || cartItems.length === 0) {
+
+    return (
       <>
-        <ProductNotFoundInCart/>
-        <ButtomContinueBuy/>
+        <ProductNotFoundInCart />
+        <ButtomContinueBuy />
       </>
     );
   }
 
+  console.log(cartItems)
+
 
   return (
     <>
-      {cartItems.map((item) => (
+      {cartItems.map((item) => {
 
-        <div className="product-cart-container" key={item.data.id}>
-          <div className="image-product-cart-container">
-            <img src={Example} alt="Imagen de una biclicleta" />
-          </div>
-          <div className="details-product-cart-container">
-            <h3>{item.data.nombre}</h3>
-            <p>Precio: {item.data.precio}</p>
-            <div className="quantity-product-cart-container">
-              <p>Cantidad:</p>
-              <div className="accountant-product-cart-container">
-                <button
-                  id="button-accountant-left"
-                  onClick={() => setCantidad(cantidad - 1)}
-                >
-                  <b>-</b>
-                </button>
-                <input type="text" value={cantidad} />
-                <button
-                  id="button-accountant-right"
-                  onClick={() => setCantidad(cantidad + 1)}
-                >
-                  <b>+</b>
-                </button>
+        
+
+        return (
+          <>
+
+            <div className="product-cart-container" key={item.data.id}>
+              <div className="image-product-cart-container">
+                <img src={Example} alt="Imagen de una biclicleta" />
               </div>
+              <div className="details-product-cart-container">
+                <h3>{item.data.nombre}</h3>
+                <p>Precio: {item.data.precio}</p>
+                <div className="quantity-product-cart-container">
+                  <p>Cantidad:</p>
+                  <div className="accountant-product-cart-container">
+                    <button
+                      id="button-accountant-left"
+                      onClick={() => handleStockChange(item.data.id, Math.max(1, parseInt(item.data.cantidad) - 1))}
+                    >
+                      <b>-</b>
+                    </button>
+                    <input type="text" value={item.data.cantidad} readOnly />
+                    <button
+                      id="button-accountant-right"
+                      onClick={() => handleStockChange(item.data.id, Math.min(item.data.stock, parseInt(item.data.cantidad) + 1))}
+                    >
+                      <b>+</b>
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <button id="button-cart-delete" onClick={() => deleteProduct(parseInt(item.data.id, 10))}>Eliminar</button>
             </div>
-          </div>
 
-          <button id="button-cart-delete" onClick={() => deleteProduct(parseInt(item.data.id, 10))}>Eliminar</button>
-          {console.log(item.data.id)}
-        </div>
-      ))}
+          </>
+        )
+      })}
 
-    <ButtomContinueBuy/>
+      <ButtomContinueBuy />
     </>
   );
 };
