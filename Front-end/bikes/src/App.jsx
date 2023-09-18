f1a327ae8359f1b8df63507f8ea33875d13f949f
Lord17777 <canegrn@misena.edu.co>
2023-09-18 08:21:14 -0500

    un pequeño cambio en el App

--- Front-end/bikes/src/App.jsx
+++ Front-end/bikes/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import { Home } from "./pages/Home";
 import "./assets/css/App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
